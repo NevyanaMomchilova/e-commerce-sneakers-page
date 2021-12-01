3da84ad86f9ea5d06e0359ac940d177043285c53
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-12-01 09:48:18 +0200

    app created,added - basic styles,images

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import '../css/App.css';
+
+// Next step - components structure, favicon???
+
+function App() {
+  return (
+    <div className="App">
+		Collections
+		Men
+		Women
+		About
+		Contact
+
+		Sneaker Company
+
+		Fall Limited Edition Sneakers
+
+		These low-profile sneakers are your perfect casual wear companion. Featuring a 
+		durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+		$125.00
+		50%
+		$250.00
+
+		0
+		Add to cart
+      	<div class="attribution">
+			Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+			Coded by <a href="https://webcodebynevyana.netlify.app/" target="_blank" rel="noreferrer">Nevyana Momchilova</a>.
+      	</div>
+    </div>
+  );
+}
+
+export default App;
