6e07a0b473a4a1fad7fd36d9e119cbc1b7da8851
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-12-08 09:00:48 +0200

    header,navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,32 +5,34 @@ const Navbar = () => {
     return (
         <header className="header">
             <BrowserRouter>
-                <div className="navbar">
-                    <NavLink to="/" className="navbar-logo">
+                <nav className="navbar">
+                    <NavLink to="/" className="navbar-logo" >
                         <img src="./images/logo.svg" alt="sneakers logo" />
                     </NavLink>
-                    <nav>
-                        <ul className="navbar-menu">
-                            <li>
-                                <NavLink to="/collections" className="navbar-menu-link">Collections</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/men" className="navbar-menu-link">Men</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/women" className="navbar-menu-link">Women</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/about" className="navbar-menu-link">About</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/contact" className="navbar-menu-link">Contact</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+                    <ul className="navbar-menu">
+                        <li>
+                            <NavLink to="/collections" className="navbar-menu-link" activeClassName="active-link" >Collections</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/men" className="navbar-menu-link" activeClassName="active-link" >Men</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/women" className="navbar-menu-link" activeClassName="active-link" >Women</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className="navbar-menu-link" activeClassName="active-link" >About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" className="navbar-menu-link"activeClassName="active-link"  >Contact</NavLink>
+                        </li>                            
+                    </ul>   
+                </nav>
                 <div className="navbar-details">
-                    <img src="./images/icon-cart.svg" alt="cart icon" className="navbar-details-cart" />
+                    <div className="navbar-details-cart-container" >
+                        <div className="navbar-details-cart-bubble" ></div>
+                        <p className="navbar-details-cart-number" >3</p>
+                        <img src="./images/icon-cart.svg" alt="cart icon" className="navbar-details-cart" />
+                    </div>
                     <img src="./images/image-avatar.png" alt="avatar" className="navbar-details-avatar" />
                 </div>
             </BrowserRouter>
