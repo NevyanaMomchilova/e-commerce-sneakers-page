ac6c87754fd86b1f6a01a2ba45953340ff561019
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-12-14 07:07:38 +0200

    ProductInformation component updated

--- src/components/App.js
+++ src/components/App.js
@@ -13,22 +13,6 @@ function App() {
 			<ProductGallery />
 			<ProductInformation />
 		</main>
-		
-
-		{/* Sneaker Company
-
-		Fall Limited Edition Sneakers
-
-		These low-profile sneakers are your perfect casual wear companion. Featuring a 
-		durable rubber outer sole, they’ll withstand everything the weather can offer.
-
-		$125.00
-		50%
-		$250.00
-
-		0
-		Add to cart */}
-		
       	<div className="attribution">
 			Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
 			Coded by <a href="https://webcodebynevyana.netlify.app/" target="_blank" rel="noreferrer">Nevyana Momchilova</a>.
--- src/components/ProductInformation.js
+++ src/components/ProductInformation.js
@@ -2,7 +2,21 @@ import "./ProductInformation.css";
 
 const ProductInformation = () => {
     return (
-        <div>Information</div>
+        
+        <div>
+            <h2>Sneaker Company</h2>
+            <h1>Fall Limited Edition Sneakers</h1>
+            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+		    durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+            <p>$125.00<span>50%</span></p>
+            <p>$250.00</p>
+            <div>
+                <button>-</button>
+                <p>0</p>
+                <button>+</button>
+            </div>
+            <button>Add to cart</button>
+        </div>
     );
 };
 
