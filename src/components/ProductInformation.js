1ddf710beda1ef17d89d6018b75cf3b7aac0ccee
Nevyana Momchilova <nevyana.dm@gmail.com>
2021-12-15 17:03:39 +0200

    ProductGallery and ProductInformation css for desktop

--- src/components/ProductInformation.js
+++ src/components/ProductInformation.js
@@ -8,14 +8,22 @@ const ProductInformation = () => {
             <h1>Fall Limited Edition Sneakers</h1>
             <p>These low-profile sneakers are your perfect casual wear companion. Featuring a 
 		    durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
-            <p>$125.00<span>50%</span></p>
-            <p>$250.00</p>
-            <div>
-                <button>-</button>
-                <p>0</p>
-                <button>+</button>
+            <div className="price-offer">
+                <p className="price-new" >$125.00</p>
+                <p className="price-discount" >50%</p>
             </div>
-            <button>Add to cart</button>
+            <div className="price-regular">
+                <p className="price-old">$250.00</p>
+                <div className="strike-through"></div>
+            </div>
+            <div className="buttons-container">
+                <div className="btn-counter">
+                    <img src="./images/icon-minus.svg" alt="minus" className="btn-counter-minus"/>
+                    <p className="btn-counter-count">0</p>
+                    <img src="./images/icon-plus.svg" alt="plus" className="btn-counter-plus" />
+                </div>
+                <button className="btn-add-to-cart">Add to cart</button>
+            </div>
         </div>
     );
 };
